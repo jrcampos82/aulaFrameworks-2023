5eb07adeef272504b2d16fb62ff65847005203f9
Beto <jrcampos82@gmail.com>
2023-03-30 14:55:12 -0400

    Aula 30-03

--- my-app/src/app/components/hero/hero.component.ts
+++ my-app/src/app/components/hero/hero.component.ts
@@ -10,12 +10,16 @@ export class HeroComponent {
 
   vetor = [1, 5, 13, 24]
 
+  hero_name = ''
+
   heroes = ["Thor", "Homem de Ferro", "Visão", "Thanos"];
 
   hero: Hero = {
     id: 1,
     nome: "Capitão América"
   }
+
+
 
 
 }
